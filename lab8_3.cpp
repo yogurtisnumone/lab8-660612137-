c808283111bb3ee57ee629ea5f946e870e24acff
kachapat_p <kachapat_p@cmu.ac.th>
2023-12-15 14:30:23 +0700

    lab2

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -11,3 +11,51 @@
 "Brook"
 "Jinbe"
 */
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int age,height,bounty;
+    cout << "Enter your age: ";
+    cin >> age;
+    
+    if(age<=25){
+        cout << "Enter your height: ";
+        cin >> height;
+        if(height<100){
+            cout << "Your character = " << "Chopper";
+        }
+        else{
+            if(height<180){
+                cout << "Your character = " << "Usopp";
+            }
+            else{
+                cout << "Enter your bounty: ";
+                cin >> bounty;
+                if(bounty>1100000000){
+                    cout << "Your character = " << "Zoro";
+                }
+                else{
+                    cout << "Your character = " << "Sanji";
+                }
+            }
+        }
+    }
+    
+    else if(age<=60){
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+        if(bounty>500000000){
+            cout << "Your character = " << "Jinbe";
+        }
+        else{
+            cout << "Your character = " << "Franky";
+        }
+    }
+    
+    else{
+        cout << "Your character = " << "Brook";
+    }
+}
+
